6534778f19fbf13923e0aeb34815cf2c35cbf166
mar <104113967+problemxtery@users.noreply.github.com>
2026-07-25 21:41:42 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector("#menu-toggle");
+const nav = document.querySelector("#nav");
+
+menuToggle.addEventListener("click", () => {
+
+    nav.classList.toggle("active");
+
+});
